d54231b9fcb2e2ca288fcb6b715d4e0a49c71889
safarov-e <elkhan.safarov10@yandex.com>
2021-03-12 16:34:30 +0500

    Вывод списка дел

--- Доска задач/src/App.js
+++ Доска задач/src/App.js
@@ -2,14 +2,21 @@ import React, {useState} from 'react';
 import './App.css';
 
 const App = () => {
-  const [board, setBoards] = useState([
+  const [boards, setBoards] = useState([
     {id: 1, title: 'Сделать', items: [{id: 1, title: 'Пойти в магазин'}, {id: 2, title: 'Выкинуть мусор'},{id: 3, title: 'Покушать'}]},
     {id: 2, title: 'Проверить', items: [{id: 4, title: 'Код ревью'}, {id: 5, title: 'Задача на факториал'},{id: 6, title: 'Задача на фибоначи'}]},
     {id: 3, title: 'Сделано', items: [{id: 7, title: 'Снять видео'}, {id: 8, title: 'Смонтировать'},{id: 9, title: 'Отрендерить'}]}
   ])
   return (
     <div className="app">
-
+      {boards.map(board =>
+        <div className="board" key={board.id}>
+          <div className="board__title">{board.title}</div>
+          {board.items.map(item => 
+            <div className="item" key={item.id}>{item.title}</div>  
+          )}
+        </div>
+      )}
     </div>
   )
 }
