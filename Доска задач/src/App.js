e6f86be3878967ad993038a9174e807937fb129a
safarov-e <elkhan.safarov10@yandex.com>
2021-03-12 18:24:42 +0500

    Отрисовка доски

--- Доска задач/src/App.js
+++ Доска задач/src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   const [boards, setBoards] = useState([
     {id: 1, title: 'Сделать', items: [{id: 1, title: 'Пойти в магазин'}, {id: 2, title: 'Выкинуть мусор'},{id: 3, title: 'Покушать'}]},
     {id: 2, title: 'Проверить', items: [{id: 4, title: 'Код ревью'}, {id: 5, title: 'Задача на факториал'},{id: 6, title: 'Задача на фибоначи'}]},
-    {id: 3, title: 'Сделано', items: [{id: 7, title: 'Снять видео'}, {id: 8, title: 'Смонтировать'},{id: 9, title: 'Отрендерить'}]}
+    {id: 3, title: 'Сделано', items: [{id: 7, title: 'Открыть редактор'}, {id: 8, title: 'Смонтировать'},{id: 9, title: 'Отрендерить'}]}
   ])
   return (
     <div className="app">
@@ -13,7 +13,18 @@ const App = () => {
         <div className="board" key={board.id}>
           <div className="board__title">{board.title}</div>
           {board.items.map(item => 
-            <div className="item" key={item.id}>{item.title}</div>  
+            <div 
+              onDragOver={(e) => dragOverHandler(e, board, item)}
+              onDragLeave={(e) => dragLeaveHandler(e)}
+              onDragStart={(e) => dragStartHandler(e)}
+              onDragEnd={(e) => dragEndHandler(e)}
+              onDrop={(e) => dropHandler(e, board, item)}
+              draggable={true}
+              className="item" 
+              key={item.id}
+            >
+              {item.title}
+            </div>  
           )}
         </div>
       )}
