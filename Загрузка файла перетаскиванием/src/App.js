b7973c22fbe0fd48ae8f0872735341ac8f2d35b3
safarov-e <elkhan.safarov10@yandex.com>
2021-03-12 19:49:18 +0500

    Стилизация

--- Загрузка файла перетаскиванием/src/App.js
+++ Загрузка файла перетаскиванием/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [drag, setDrag] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {drag
+        ? <div className="drop-area">Отпустите файлы, чтобы загрузить их</div>
+        : <div>Перетащите файлы, чтобы загрузить их</div>}
     </div>
-  );
+  )
 }
 
 export default App;
