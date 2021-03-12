db55b47bb0b227977c6e2c38cf01d61ea64ea8ae
safarov-e <elkhan.safarov10@yandex.com>
2021-03-12 20:55:25 +0500

    Смена состояние

--- Загрузка файла перетаскиванием/src/App.js
+++ Загрузка файла перетаскиванием/src/App.js
@@ -2,13 +2,21 @@ import React, {useState} from 'react';
 import './App.css';
 
 const App = () => {
-  const [drag, setDrag] = useState(true)
+  const [drag, setDrag] = useState(false)
 
+  function dragStartHandler(e) {
+    e.preventDefault();
+    setDrag(true);
+  }
   return (
     <div className="app">
       {drag
         ? <div className="drop-area">Отпустите файлы, чтобы загрузить их</div>
-        : <div>Перетащите файлы, чтобы загрузить их</div>}
+        : <div
+            onDragStart={e => dragStartHandler(e)}
+            onDragLeave={e => }
+            onDragOver={e => dragStartHandler(e)}
+          >Перетащите файлы, чтобы загрузить их</div>}
     </div>
   )
 }
